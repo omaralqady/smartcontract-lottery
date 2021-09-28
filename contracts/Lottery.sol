2d7afc250b91862f0d19e6455c4835963118b6c4
Omar Alqady <omar@founda.com>
2021-09-28 15:37:19 +0200

    feat: start lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,20 +1,30 @@
 // SDPX-License-Identifier: MIT
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 pragma solidity ^0.6.6;
 
-contract Lottery {
+enum LOTTERY_STATE {
+    OPEN,
+    CLOSED,
+    CALCULATING_WINNER
+}
+
+contract Lottery is Ownable {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface public priceFeed;
+    LOTTERY_STATE public lotteryState;
 
     constructor(address _priceFreed) public {
         usdEntryFee = 50 * (10**18);
         priceFeed = AggregatorV3Interface(_priceFreed);
+        lotteryState = LOTTERY_STATE.CLOSED;
     }
 
     function enter() public payable {
+        require(lotteryState == LOTTERY_STATE.OPEN);
         require(msg.value >= getEntranceFee(), "Not enough ETH!");
         players.push(msg.sender);
     }
@@ -24,5 +34,13 @@ contract Lottery {
         uint256 adjustedPrice = uint256(price) * (10**18);
         uint256 fee = (usdEntryFee * 10**18) / adjustedPrice;
         return fee;
+    }
+
+    function startLottery() public onlyOwner {
+        require(
+            lotteryState == LOTTERY_STATE.CLOSED,
+            "Cannot start new lottery yet!"
+        );
+        lotteryState = LOTTERY_STATE.OPEN;
     }
 }
