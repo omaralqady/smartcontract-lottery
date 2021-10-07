5fcd2a852f13d11b6b930686a9c8e968ab7f3b32
Omar Alqady <omar@founda.com>
2021-10-08 00:03:13 +0200

    feat: end lottery and randomly pick winner

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,6 +1,7 @@
 // SDPX-License-Identifier: MIT
 
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 pragma solidity ^0.6.6;
@@ -11,16 +12,28 @@ enum LOTTERY_STATE {
     CALCULATING_WINNER
 }
 
-contract Lottery is Ownable {
+contract Lottery is VRFConsumerBase, Ownable {
     address payable[] public players;
     uint256 public usdEntryFee;
     AggregatorV3Interface public priceFeed;
     LOTTERY_STATE public lotteryState;
+    uint256 public fee;
+    uint256 public randomness;
+    bytes32 public keyhash;
+    address payable public winner;
 
-    constructor(address _priceFreed) public {
+    constructor(
+        address _priceFreed,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyhash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * (10**18);
         priceFeed = AggregatorV3Interface(_priceFreed);
         lotteryState = LOTTERY_STATE.CLOSED;
+        fee = _fee;
+        keyhash = _keyhash;
     }
 
     function enter() public payable {
@@ -32,8 +45,8 @@ contract Lottery is Ownable {
     function getEntranceFee() public view returns (uint256) {
         (, int256 price, , , ) = priceFeed.latestRoundData();
         uint256 adjustedPrice = uint256(price) * (10**18);
-        uint256 fee = (usdEntryFee * 10**18) / adjustedPrice;
-        return fee;
+        uint256 feeValue = (usdEntryFee * 10**18) / adjustedPrice;
+        return feeValue;
     }
 
     function startLottery() public onlyOwner {
@@ -42,5 +55,30 @@ contract Lottery is Ownable {
             "Cannot start new lottery yet!"
         );
         lotteryState = LOTTERY_STATE.OPEN;
+    }
+
+    function endLottery() public onlyOwner {
+        lotteryState = LOTTERY_STATE.CALCULATING_WINNER;
+        bytes32 requestId = requestRandomness(keyhash, fee);
+    }
+
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lotteryState == LOTTERY_STATE.CALCULATING_WINNER,
+            "You aren't there yet!"
+        );
+        require(_randomness > 0, "random-not-found");
+
+        uint256 indexWinner = _randomness % players.length;
+        winner = players[indexWinner];
+        winner.transfer(address(this).balance);
+
+        // reset lottery
+        players = new address payable[](0);
+        lotteryState = LOTTERY_STATE.CLOSED;
+        randomness = _randomness;
     }
 }
